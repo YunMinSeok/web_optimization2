3e1186a5f2259a840d8a338346bca920c18aa882
윤민석 <ykmvm147@naver.com>
2023-01-28 15:49:52 +0900

    feat: intersectionObserver 예제 구현 완료

--- lecture-3/src/components/Card.js
+++ lecture-3/src/components/Card.js
@@ -1,14 +1,26 @@
-import React from 'react'
+import React, { useEffect, useRef } from "react";
 
 function Card(props) {
-	return (
-		<div className="Card text-center">
-			<img src={props.image}/>
-			<div className="p-5 font-semibold text-gray-700 text-xl md:text-lg lg:text-xl keep-all">
-				{props.children}
-			</div>
-		</div>
-	)
+  const imgRef = useRef();
+
+  useEffect(() => {
+    const options = {};
+    const callback = () => {
+      console.log("callback");
+    };
+    const observer = new IntersectionObserver(callback, options);
+
+    observer.observe(imgRef.current);
+  }, []);
+
+  return (
+    <div className="Card text-center" ref={imgRef}>
+      <img src={props.image} />
+      <div className="p-5 font-semibold text-gray-700 text-xl md:text-lg lg:text-xl keep-all">
+        {props.children}
+      </div>
+    </div>
+  );
 }
 
-export default Card
+export default Card;
