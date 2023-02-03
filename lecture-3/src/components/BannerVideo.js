70511c29e044759f5a47ca252e790f91c9e15f21
윤민석 <ykmvm147@naver.com>
2023-02-03 16:26:09 +0900

    feat: font observer 실전코드 완료

--- lecture-3/src/components/BannerVideo.js
+++ lecture-3/src/components/BannerVideo.js
@@ -1,8 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import video from "../assets/banner-video.mp4";
 import video_webm from "../assets/banner-video.webm";
+import FontFaceObserver from "fontfaceobserver";
 
 function BannerVideo() {
+  const [isFontLoaded, setIsFontLoaded] = useState(false);
+  var font = new FontFaceObserver("BMYEONSUNG");
+
+  font.load().then(function () {
+    console.log("BMTEONSUNG has load");
+	setIsFontLoaded(true);
+  });
+
   return (
     <div className="BannerVideo w-full h-screen overflow-hidden relative bg-texture">
       <div className="absolute h-screen w-full left-1/2">
@@ -16,7 +25,7 @@ function BannerVideo() {
           <source src={video} type="video/mp4" />
         </video>
       </div>
-      <div className="w-full h-full flex justify-center items-center">
+      <div className="w-full h-full flex justify-center items-center" style={}>
         <div className="text-white text-center">
           <div className="text-6xl leading-none font-semibold">KEEP</div>
           <div className="text-6xl leading-none font-semibold">CALM</div>
