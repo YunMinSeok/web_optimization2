57bc3394a0abcca93310014b2dde2e7a71f43fbc
윤민석 <ykmvm147@naver.com>
2023-02-19 17:18:02 +0900

    feat: selector 함수로 나누기

--- lecture-4-master/src/containers/PhotoListContainer.js
+++ lecture-4-master/src/containers/PhotoListContainer.js
@@ -3,6 +3,7 @@ import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import PhotoList from '../components/PhotoList';
 import { fetchPhotos } from '../redux/photos';
+import selectFilteredPhotos from '../redux/selector/selectFilteredPhotos';
 
 function PhotoListContainer() {
   const dispatch = useDispatch();
@@ -11,13 +12,7 @@ function PhotoListContainer() {
     dispatch(fetchPhotos());
   }, [dispatch]);
 
-  const selecterFilteredPhostos = createSelector(
-    [state => state.photos.data, state => state.category.category],
-    (photos, category) =>
-      category === 'all' ? photos : photos.filter(photo => photo.category === category)
-  );
-
-  const photos = useSelector(selecterFilteredPhostos);
+  const photos = useSelector(selectFilteredPhotos);
   const loading = useSelector(state => state.photos.loading);
 
   if (loading === 'error') {
