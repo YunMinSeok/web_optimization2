958c8c9a198a957a7342c3d66fc513eb85673adf
윤민석 <ykmvm147@naver.com>
2023-02-19 16:43:26 +0900

    feat: shalloEqual 적용 완료

--- lecture-4-master/src/containers/ImageModalContainer.js
+++ lecture-4-master/src/containers/ImageModalContainer.js
@@ -1,15 +1,25 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useSelector, shallowEqual } from 'react-redux';
 import ImageModal from '../components/ImageModal';
 
 function ImageModalContainer() {
-  const modalVisible = useSelector(state => state.imageModal.modalVisible);
+  // const modalVisible = useSelector(state => state.imageModal.modalVisible);
 
-  const bgColor = useSelector(state => state.imageModal.bgColor);
+  // const bgColor = useSelector(state => state.imageModal.bgColor);
 
-  const src = useSelector(state => state.imageModal.src);
+  // const src = useSelector(state => state.imageModal.src);
 
-  const alt = useSelector(state => state.imageModal.alt);
+  // const alt = useSelector(state => state.imageModal.alt);
+
+  const { modalVisible, bgColor, src, alt } = useSelector(
+    state => ({
+      modalVisible: state.imageModal.modalVisible,
+      bgColor: state.imageModal.bgColor,
+      src: state.imageModal.src,
+      alt: state.imageModal.alt,
+    }),
+    shallowEqual
+  );
 
   return <ImageModal modalVisible={modalVisible} bgColor={bgColor} src={src} alt={alt} />;
 }
